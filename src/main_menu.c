238e7b36d0d7ebe464fadd72291e685699698b48
D-works <dhjustchillin@gmail.com>
2026-08-14 23:50:35 -0500

    auto sync

--- src/main_menu.c
+++ src/main_menu.c
@@ -1420,7 +1420,7 @@ static void Task_NewGameOliveSpeechSub_InitPokeBall(u8 taskId)
     gSprites[spriteId].invisible = FALSE;
     gSprites[spriteId].data[0] = 0;
 
-    CreatePokeballSpriteToReleaseMon(spriteId, gSprites[spriteId].oam.paletteNum, 112, 58, 0, 0, 32, PALETTES_BG, SPECIES_CINDERF);
+    CreatePokeballSpriteToReleaseMon(spriteId, gSprites[spriteId].oam.paletteNum, 137, 46, 0, 0, 32, PALETTES_BG, SPECIES_CINDERF);
     gTasks[taskId].func = Task_NewGameOliveSpeechSub_WaitForCINDERF;
     gTasks[sOliveSpeechMainTaskId].tTimer = 0;
 }
