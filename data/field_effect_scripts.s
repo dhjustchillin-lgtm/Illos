804f602ff3fc75a9fe15e9c8a0eb77c317fc5d84
D-works <dhjustchillin@gmail.com>
2026-08-29 11:48:00 -0500

    auto sync

--- data/field_effect_scripts.s
+++ data/field_effect_scripts.s
@@ -158,7 +158,7 @@ gFieldEffectScript_JumpSmallSplash::
 	field_eff_end
 
 gFieldEffectScript_LongGrass::
-	field_eff_loadfadedpal_callnative gSpritePalette_GeneralFieldEffect1, FldEff_LongGrass
+	field_eff_loadfadedpal_callnative gSpritePalette_Wheat, FldEff_LongGrass
 	field_eff_end
 
 gFieldEffectScript_JumpLongGrass::
--- src/data/field_effects/field_effect_objects.h
+++ src/data/field_effects/field_effect_objects.h
@@ -1,5 +1,7 @@
 const struct SpritePalette gSpritePalette_GeneralFieldEffect0 = {gFieldEffectObjectPalette0, FLDEFF_PAL_TAG_GENERAL_0};
 const struct SpritePalette gSpritePalette_GeneralFieldEffect1 = {gFieldEffectObjectPalette1, FLDEFF_PAL_TAG_GENERAL_1};
+const struct SpritePalette gSpritePalette_Wheat = {gFieldEffectPal_Wheat, FLDEFF_PAL_TAG_WHEAT};
+
 
 static const union AnimCmd sAnim_Shadow[] =
 {
@@ -709,7 +711,7 @@ static const union AnimCmd *const sAnimTable_LongGrass[] =
 
 const struct SpriteTemplate gFieldEffectObjectTemplate_LongGrass = {
     .tileTag = TAG_NONE,
-    .paletteTag = FLDEFF_PAL_TAG_GENERAL_1,
+    .paletteTag = FLDEFF_PAL_TAG_WHEAT,
     .oam = &gObjectEventBaseOam_16x16,
     .anims = sAnimTable_LongGrass,
     .images = sPicTable_LongGrass,
